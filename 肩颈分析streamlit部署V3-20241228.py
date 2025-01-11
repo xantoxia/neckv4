cfe9a9e316bfb4ff8124e2c518fc2fddc7ca4a89
ergo <traderxia@qq.com>
2025-01-11 10:12:11 +0800

    Update 肩颈分析streamlit部署V3-20241228.py

--- 肩颈分析streamlit部署V3-20241228.py
+++ 肩颈分析streamlit部署V3-20241228.py
@@ -37,6 +37,10 @@ model_filename = f"jjmodel{timestamp}.joblib"
 
 # 保存和上传新模型
 def save_and_upload_new_model(model, model_filename, commit_message):
+    try:
+    g = Github(token)
+    repo = g.get_repo(repo_name)
+    
     # 保存新模型到临时文件夹
     local_model_path = f"/tmp/{model_filename}"
     dump(model, local_model_path)

82b31ac5f83eb3d4258c5f29f7d6681157443300
ergo <traderxia@qq.com>
2025-01-14 10:19:57 +0800

    Update 肩颈分析streamlit部署V3-20241228.py

--- 肩颈分析streamlit部署V3-20241228.py
+++ 肩颈分析streamlit部署V3-20241228.py
@@ -538,7 +538,7 @@ if uploaded_file is not None:
     st.write("模型已训练并保存到本地临时路径。") 
     
     # 上传新模型到 GitHub
-    save_and_upload_new_model(local_model_path, models_folder + model_filename, commit_message)
+    save_and_upload_new_model(model, model_filename, commit_message)
     st.write("模型已保存并上传到 GitHub。")
 
     # 更新最新模型信息

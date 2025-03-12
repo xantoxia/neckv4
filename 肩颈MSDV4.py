d5edaeef227445d8efadbec4dacb56e9d2b33891
ergo <traderxia@qq.com>
2025-03-12 15:26:21 +0800

    Update 肩颈MSDV4.py

--- 肩颈MSDV4.py
+++ 肩颈MSDV4.py
@@ -516,7 +516,7 @@ if uploaded_file is not None:
     st.write("模型已训练并保存到本地临时路径。")
 
     # 上传新模型到 GitHub
-    save_and_upload_new_model(local_model_path, f"{MODELS_DIR}MSD-{timestamp}.joblib")
+    save_and_upload_new_model(local_model_path, f"{MODELS_DIR}MSD-{timestamp}.joblib",COMMIT_MSG_MODEL)
     st.write("模型已保存并上传到 GitHub。")
     
     st.write("#### 页面导出")

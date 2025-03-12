1785f34a9f81fd22484df6f52a95ca4fbbfceae1
ergo <traderxia@qq.com>
2025-03-12 14:35:05 +0800

    Update 肩颈MSDV4.py

--- 肩颈MSDV4.py
+++ 肩颈MSDV4.py
@@ -41,8 +41,8 @@ model_filename = f"MSD-{timestamp}.joblib"
 def upload_model_to_github(file_path, github_path, commit_msg=COMMIT_MSG_MODEL):
    # """模型文件专用上传函数（保留原有逻辑）"""
     try:
-        g = Github(os.getenv("GITHUB_TOKEN"))
-        repo = g.get_repo(repo_name)
+        g = Github(os.getenv("GITHUB_TOKEN1"))
+        repo = g.get_repo(repo_name1)
         
         with open(file_path, "rb") as f:
             content = f.read()
@@ -63,8 +63,8 @@ def upload_model_to_github(file_path, github_path, commit_msg=COMMIT_MSG_MODEL):
 # 下载最新模型文件
 def download_latest_model_from_github():
     try:
-        g = Github(token)
-        repo = g.get_repo(repo_name)
+        g = Github(os.getenv("GITHUB_TOKEN1"))
+        repo = g.get_repo(repo_name1)
 
         # 获取最新模型信息
         try:
